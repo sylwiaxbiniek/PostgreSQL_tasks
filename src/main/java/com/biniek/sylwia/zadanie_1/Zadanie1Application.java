3cbfad5627ef4fdff9fa218ec8892dfa198597d4
Sylwia Biniek <sylwiaxbiniek@gmail.com>
2020-01-20 20:34:58 +0100

    Stworzenie pliku bazy danych w biezacym katalogu

--- src/main/java/com/biniek/sylwia/zadanie_1/Zadanie1Application.java
+++ src/main/java/com/biniek/sylwia/zadanie_1/Zadanie1Application.java
@@ -11,23 +11,24 @@ import java.sql.SQLException;
 
 public class Zadanie1Application {
 
-//	public static void createNewDatabase(String fileName) {
+	public static void createNewDatabase(String fileName) {
 
-    //     String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
-		//
-    //     try (Connection conn = DriverManager.getConnection(url)) {
-    //         if (conn != null) {
-    //             DatabaseMetaData meta = conn.getMetaData();
-    //             System.out.println("The driver name is " + meta.getDriverName());
-    //             System.out.println("A new database has been created.");
-    //         }
-		//
-    //     } catch (SQLException e) {
-    //         System.out.println(e.getMessage());
-    //     }
-    // }
+        String url = "jdbc:sqlite:" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 	public static void main(String[] args) {
+		createNewDatabase("test.db");
 
 		List<DaneWejsciowe> uslugi = new ArrayList<>();
 		List<KategoriePodsumowania> kategorie = new ArrayList<>();
@@ -43,30 +44,18 @@ public class Zadanie1Application {
 		kategorie.add(new KategoriePodsumowania(2, "Telewizja", 3));
 		kategorie.add(new KategoriePodsumowania(3, "Uslugi dodatkowe", 2));
 
-		/*KategoriePodsumowania internet = new KategoriePodsumowania(1, "Internet", 1);
-		KategoriePodsumowania telewizja = new KategoriePodsumowania(2, "Telewizja", 3);
-		KategoriePodsumowania uslugi_dodatkowe = new KategoriePodsumowania(3, "Uslugi dodatkowe", 2);
-
-		DaneWejsciowe abo_internet = new DaneWejsciowe("Abonament za internet", 40.00, 1);
-		DaneWejsciowe bezp_internet = new DaneWejsciowe("Bezpieczny internet", 9.90, 3);
-		DaneWejsciowe pakiet_pelny = new DaneWejsciowe("Telewizja pakiet pelny", 80.00, 2);
-		DaneWejsciowe nagrywarka = new DaneWejsciowe("GigaNagrywarka", 15.00, 2);
-		DaneWejsciowe rabat = new DaneWejsciowe("Rabat za internet", -20.00, 1);
-		*/
-
 		for(DaneWejsciowe usluga:uslugi){
 				int id_uslugi = usluga.id_kategorii;
 				KategoriePodsumowania kategoria = kategorie.get(id_uslugi-1);
 				kategoria.wartosc += usluga.cenaUslugi;
-			}
+		}
 
-			kategorie.sort((a, b) -> a.kolejnoscSortowania - b.kolejnoscSortowania);
+		kategorie.sort((a, b) -> a.kolejnoscSortowania - b.kolejnoscSortowania);
 
-			for (KategoriePodsumowania kategoria : kategorie) {
+		for (KategoriePodsumowania kategoria : kategorie) {
 			pods.add(new Podsumowanie(kategoria.nazwaKategorii, kategoria.wartosc));
 			//baza_danych.save(new Podsumowanie(kategoria.nazwaKategorii, kategoria.wartosc));
 		}
-		//createNewDatabase("test.db");
 
 		System.out.println(pods);
 	}
